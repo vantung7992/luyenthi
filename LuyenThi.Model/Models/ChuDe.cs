67bd7094fb87ec47c138646d73e25d02f220cdeb
TUNGNV\mrtung <vantung7992@gmail.com>
2017-09-15 17:20:21 +0700

    Doi y

--- LuyenThi.Model/Models/ChuDe.cs
+++ LuyenThi.Model/Models/ChuDe.cs
@@ -1,11 +1,12 @@
 ﻿using LuyenThi.Model.Abstract;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LuyenThi.Model.Models
 {
-    [Table("Chude")]
-    public class Chude : Auditable
+    [Table("Topics")]
+    public class Toppic : Auditable
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -17,19 +18,23 @@ namespace LuyenThi.Model.Models
 
         public int? IDChudeCha { get; set; }
 
+        [MaxLength(200)]
+        public string Seo { get; set; }
+
         [Column(TypeName = "varchar")]
         [MaxLength(200)]
         public string Tag { get; set; }
 
-        [MaxLength(200)]
-        public string Seo { get; set; }
-
-        [MaxLength(1000)]
+        [MaxLength(500)]
         public string Mota { get; set; }
 
         [MaxLength(256)]
         public string Image { get; set; }
 
-        public bool HomeFlag { get; set; }
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { get; set; }
+
+        public virtual IEnumerable<Cauhoi> DanhsachCauhoi { get; set; }
     }
 }
